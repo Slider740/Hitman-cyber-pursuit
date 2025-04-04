91cf58973a6287168eecae4e3957a7c85c49536c
Slider740 <97116291@st.deltion.nl>
2025-04-04 15:24:04 +0200

    comments toegevoegd aan inventory scripts

--- periode 2/Assets/Scripts/Inventory test 2/Inventory.cs
+++ periode 2/Assets/Scripts/Inventory test 2/Inventory.cs
@@ -5,54 +5,57 @@ using UnityEngine.UIElements;
 
 public class Inventory : MonoBehaviour
 {
-    public List <Weapon> items = new List <Weapon> ();
-    public GameObject inventoryUI;
+    public List <Weapon> items = new List <Weapon> (); //list of all items in the inventory
+    public GameObject inventoryUI; //Reference to the inventory's UI
     public Transform playerHand; //Reference to the players hand transform
-    private bool isInventoryOpen = false;
+    private bool isInventoryOpen = false; //checks if the inventory is open
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown (KeyCode.Tab))
         {
-            isInventoryOpen = !isInventoryOpen;
-            inventoryUI.SetActive(isInventoryOpen);
+            isInventoryOpen = !isInventoryOpen; //checks if the inventory is open
+            inventoryUI.SetActive(isInventoryOpen); //activates the inventory UI
         }
     }
 
     public void Additem(Weapon weapon)
     {
-        items.Add (weapon);
-        UpdateInventoryUI();
+        items.Add (weapon); //adds the item to the inventory list
+        UpdateInventoryUI(); //updates the inventory UI
     }
 
     public void EquipWeapon(Weapon weapon)
     {
-        GameObject equippedWeapon = Instantiate(weapon.gameObject, playerHand.position, playerHand.rotation, playerHand);
-        equippedWeapon.SetActive(true);
+        GameObject equippedWeapon = Instantiate(weapon.gameObject, playerHand.position, playerHand.rotation, playerHand); //places weapon in the 'hand' object
+        equippedWeapon.SetActive(true); //sets the weapon active in the world space
     }
 
     public void RemoveItem(Weapon weapon)
     {
-        items.Remove(weapon);
-        UpdateInventoryUI();
+        items.Remove(weapon); //removes the item from the inventory list
+        UpdateInventoryUI(); //updates the inventory UI
     }
 
+    //Updates the inventory UI
     public void UpdateInventoryUI()
     {
+        //places the weapon sprite in the inventory UI
         foreach (Transform child in inventoryUI.transform)
         {
-            Destroy(child.gameObject);
+            Destroy(child.gameObject); //deletes the weapon from world space
         }
+        //every weapon gets added to the Items list
         foreach (Weapon weapon in items)
         {
             GameObject newItem = new GameObject();
-            newItem.transform.SetParent(inventoryUI.transform);
+            newItem.transform.SetParent(inventoryUI.transform); //sets the parent of the newitem
 
-            var button = newItem.AddComponent<UnityEngine.UI.Button>();
-            newItem.AddComponent<UnityEngine.UI.Image>().sprite = weapon.weaponIcon;
+            var button = newItem.AddComponent<UnityEngine.UI.Button>(); //creates button from weapon sprite
+            newItem.AddComponent<UnityEngine.UI.Image>().sprite = weapon.weaponIcon; //uses the weapon sprite for the button
 
-            button.onClick.AddListener(() => EquipWeapon (weapon));
+            button.onClick.AddListener(() => EquipWeapon (weapon)); //makes the button interactable
         }
     }
 }
--- periode 2/Assets/Scripts/Inventory test 2/WeaponPickup.cs
+++ periode 2/Assets/Scripts/Inventory test 2/WeaponPickup.cs
@@ -5,25 +5,25 @@ using UnityEngine;
 public class WeaponPickup : MonoBehaviour
 {
     public Inventory inventory; // Reference to the inventory script
-    public KeyCode pickupKey = KeyCode.E;
+    public KeyCode pickupKey = KeyCode.E; //refers to E
 
     public void Start()
     {
-        if (inventory == null)
+        if (inventory == null) //checks if the inventory is equal to zero
         {
-            inventory = FindObjectOfType<Inventory>();
+            inventory = FindObjectOfType<Inventory>(); //if there is no inventory assigned in the inspector, it will search for a something eqaul to inventory
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player") && Input.GetKeyUp(pickupKey))
+        if (other.CompareTag("Player") && Input.GetKeyUp(pickupKey)) //checks for tag 'Player' and checks for input of the 'pickupkey'
         {
-            Weapon weapon = GetComponent<Weapon>();
-            if (inventory != null)
+            Weapon weapon = GetComponent<Weapon>(); //uses the script 'Weapon'
+            if (inventory != null) //checks if the inventory is equal to zero
             {
-                inventory.Additem(weapon);
-                gameObject.SetActive(false);
+                inventory.Additem(weapon); //adds the weapon to the inventory
+                gameObject.SetActive(false); //sets the weapon inactive in the world space
             }
 
         }
